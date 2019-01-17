7565d80e1a4c9e3fdafe5d7af7757cd47ebcb8d2
qfstudy <qifeistudy@gmail.com>
2019-01-17 23:21:09 +0800

    新建service文件夹，连接mongodb

---
+++ service/index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa')
+const app = new Koa()
+const {connect} = require('./database/init.js')
+
+;(async () =>{
+  await connect()
+})()
+
+
+app.use(async(ctx)=>{
+  ctx.body='<h1>hello koa</h1>'
+})
+
+app.listen(3000,()=>{
+  console.log('server 3000')
+})
